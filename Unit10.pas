72d59fe3bc23d940aeab9423da85bf16598c5eef
luiztw <lipetw66@gmail.com>
2022-05-02 18:16:42 -0300

    first commit

---
+++ Unit10.pas
@@ -0,0 +1,26 @@
+unit Unit10;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.Imaging.pngimage, Vcl.ExtCtrls;
+
+type
+  Tfrm_C = class(TForm)
+    Image1: TImage;
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  frm_C: Tfrm_C;
+
+implementation
+
+{$R *.dfm}
+uses Unit2, Unit3, Unit4, unit5, unit6, unit7, unit8, unit9;
+
+end.
---
+++ Unit8.pas
@@ -0,0 +1,27 @@
+unit Unit8;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.ExtCtrls, Vcl.Imaging.pngimage;
+
+type
+  Tfrm_I = class(TForm)
+    Image1: TImage;
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  frm_I: Tfrm_I;
+
+implementation
+
+{$R *.dfm}
+
+uses Unit2, Unit3, Unit4, unit5, unit6, unit7, unit9, unit10;
+
+end.
